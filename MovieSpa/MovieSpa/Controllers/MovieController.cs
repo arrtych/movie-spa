97d6dd015915b65bc3948a0776928f48fea09685
Artur Lipin <artur.lipin@ttu.ee>
2018-01-20 18:30:55 +0200

    create simple objects

---
+++ MovieSpa/MovieSpa/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using MovieSpa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieSpa.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult List()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie()
+            {
+                Id = 1,
+                Title = "New title1",
+                Genre = "comedy",
+                Year = 1985,
+                Rating = 7.8,
+                Description = "lorem impsum"
+
+
+            });
+            movies.Add(new Movie()
+            {
+                Id = 2,
+                Title = "New title 2",
+                Genre = "comedy",
+                Year = 2007,
+                Rating = 4.075,
+                Description = "lorem impsum"
+
+
+            });
+            movies.Add(new Movie()
+            {
+                Id = 2,
+                Title = "New title",
+                Genre = "comedy",
+                Year = 1965,
+                Rating = 5,
+                Description = "lorem impsum"
+
+
+            });
+
+            //returns from Views/Movie/List
+            return View(movies);
+        }
+    }
+}
