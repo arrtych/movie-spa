dc4035946c914183c4ef44eadef25f095fd9291e
Artur Lipin <artur.lipin@ttu.ee>
2018-01-22 03:02:22 +0200

    modified index page

--- MovieSpa/MovieSpa/Controllers/HomeController.cs
+++ MovieSpa/MovieSpa/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieSpa.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,46 @@ namespace MovieSpa.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Movie
         public ActionResult Index()
         {
-            return View();
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie()
+            {
+                Id = 1,
+                Title = "New title1",
+                Genre = "comedy",
+                Year = 1985,
+                Rating = 7.8,
+                Description = "lorem impsum"
+
+
+            });
+            movies.Add(new Movie()
+            {
+                Id = 2,
+                Title = "New title 2",
+                Genre = "comedy",
+                Year = 2007,
+                Rating = 4.075,
+                Description = "lorem impsum"
+
+
+            });
+            movies.Add(new Movie()
+            {
+                Id = 2,
+                Title = "New title",
+                Genre = "comedy",
+                Year = 1965,
+                Rating = 5,
+                Description = "lorem impsum"
+
+
+            });
+
+            //returns from Views/Movie/List
+            return View(movies);
         }
 
         public ActionResult About()
--- MovieSpa/MovieSpa/Controllers/MovieController.cs
+++ MovieSpa/MovieSpa/Controllers/MovieController.cs
@@ -9,46 +9,46 @@ namespace MovieSpa.Controllers
 {
     public class MovieController : Controller
     {
-        // GET: Movie
-        public ActionResult List()
-        {
-            List<Movie> movies = new List<Movie>();
-            movies.Add(new Movie()
-            {
-                Id = 1,
-                Title = "New title1",
-                Genre = "comedy",
-                Year = 1985,
-                Rating = 7.8,
-                Description = "lorem impsum"
+        //// GET: Movie
+        //public ActionResult Index()
+        //{
+        //    List<Movie> movies = new List<Movie>();
+        //    movies.Add(new Movie()
+        //    {
+        //        Id = 1,
+        //        Title = "New title1",
+        //        Genre = "comedy",
+        //        Year = 1985,
+        //        Rating = 7.8,
+        //        Description = "lorem impsum"
 
 
-            });
-            movies.Add(new Movie()
-            {
-                Id = 2,
-                Title = "New title 2",
-                Genre = "comedy",
-                Year = 2007,
-                Rating = 4.075,
-                Description = "lorem impsum"
+        //    });
+        //    movies.Add(new Movie()
+        //    {
+        //        Id = 2,
+        //        Title = "New title 2",
+        //        Genre = "comedy",
+        //        Year = 2007,
+        //        Rating = 4.075,
+        //        Description = "lorem impsum"
 
 
-            });
-            movies.Add(new Movie()
-            {
-                Id = 2,
-                Title = "New title",
-                Genre = "comedy",
-                Year = 1965,
-                Rating = 5,
-                Description = "lorem impsum"
+        //    });
+        //    movies.Add(new Movie()
+        //    {
+        //        Id = 2,
+        //        Title = "New title",
+        //        Genre = "comedy",
+        //        Year = 1965,
+        //        Rating = 5,
+        //        Description = "lorem impsum"
 
 
-            });
+        //    });
 
-            //returns from Views/Movie/List
-            return View(movies);
-        }
+        //    //returns from Views/Movie/List
+        //    return View(movies);
+        //}
     }
 }
